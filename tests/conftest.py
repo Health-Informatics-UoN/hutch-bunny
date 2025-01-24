f0f39e168a6a24cd306423c46678fa4c1e106f8d
emlyn <emlyn@elementech.co.uk>
2025-01-24 20:56:48 +0000

    Updates from main

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,9 @@ from hutch_bunny.core.rquest_dto.rule import Rule
 from hutch_bunny.core.db_manager import SyncDBManager
 from hutch_bunny.core.query_solvers import AvailabilityQuery, DistributionQuery
 import hutch_bunny.core.settings as settings
+import hutch_bunny.core.setting_database as db_settings
 
+"""
 @pytest.fixture
 def db_manager():
     datasource_db_port = os.getenv("DATASOURCE_DB_PORT")
@@ -18,6 +20,22 @@ def db_manager():
         port=(int(datasource_db_port) if datasource_db_port is not None else None),
         database=os.getenv("DATASOURCE_DB_DATABASE"),
         drivername=os.getenv("DATASOURCE_DB_DRIVERNAME", settings.DEFAULT_DB_DRIVER),
+        schema=os.getenv("DATASOURCE_DB_SCHEMA"),
+    )
+"""
+
+@pytest.fixture
+def db_manager():
+    datasource_db_port = os.getenv("DATASOURCE_DB_PORT")
+    return SyncDBManager(
+        username=os.getenv("DATASOURCE_DB_USERNAME"),
+        password=os.getenv("DATASOURCE_DB_PASSWORD"),
+        host=os.getenv("DATASOURCE_DB_HOST"),
+        port=(int(datasource_db_port) if datasource_db_port is not None else None),
+        database=os.getenv("DATASOURCE_DB_DATABASE"),
+        drivername=db_settings.expand_short_drivers(
+            os.getenv("DATASOURCE_DB_DRIVERNAME", settings.DEFAULT_DB_DRIVER)
+        ),
         schema=os.getenv("DATASOURCE_DB_SCHEMA"),
     )
 
@@ -152,3 +170,4 @@ def distribution_query():
         uuid="unique_id",
         collection="collection_id",
     )
+
