98bf55e1580c5c21c8833ae43183bf574e6426dd
vpnu <vasiliki.panagi@nottingham.ac.uk>
2025-01-24 10:06:33 +0000

    replace nan with empty string

--- src/hutch_bunny/core/query_solvers.py
+++ src/hutch_bunny/core/query_solvers.py
@@ -302,7 +302,8 @@ class CodeDistributionQuerySolver(BaseDistributionQuerySolver):
         )
         for _, row in concepts_df.iterrows():
             df.loc[df["OMOP"] == row["concept_id"], "OMOP_DESCR"] = row["concept_name"]
-
+        # replace NaN values with empty string
+        df = df.fillna('')
         # Convert df to tab separated string
         results = list(["\t".join(df.columns)])
         for _, row in df.iterrows():
@@ -481,10 +482,12 @@ def solve_distribution(
     solver = _get_distribution_solver(db_manager, query)
     try:
         res, count = solver.solve_query()
+        print(res)
         # Convert file data to base64
         res_b64_bytes = base64.b64encode(res.encode("utf-8"))  # bytes
         size = len(res_b64_bytes) / 1000  # length of file data in KB
         res_b64 = res_b64_bytes.decode("utf-8")  # convert back to string, now base64
+
         result_file = File(
             data=res_b64,
             description="Result of code.distribution anaylsis",
