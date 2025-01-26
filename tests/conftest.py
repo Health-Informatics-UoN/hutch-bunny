7bb7c2813aa5e344ce440a90bf0fb92b40fbb54f
emlyn <emlyn@elementech.co.uk>
2025-01-26 17:52:58 +0000

    Removed commented old db_manager

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,21 +9,6 @@ from hutch_bunny.core.query_solvers import AvailabilityQuery, DistributionQuery
 import hutch_bunny.core.settings as settings
 import hutch_bunny.core.setting_database as db_settings
 
-"""
-@pytest.fixture
-def db_manager():
-    datasource_db_port = os.getenv("DATASOURCE_DB_PORT")
-    return SyncDBManager(
-        username=os.getenv("DATASOURCE_DB_USERNAME"),
-        password=os.getenv("DATASOURCE_DB_PASSWORD"),
-        host=os.getenv("DATASOURCE_DB_HOST"),
-        port=(int(datasource_db_port) if datasource_db_port is not None else None),
-        database=os.getenv("DATASOURCE_DB_DATABASE"),
-        drivername=os.getenv("DATASOURCE_DB_DRIVERNAME", settings.DEFAULT_DB_DRIVER),
-        schema=os.getenv("DATASOURCE_DB_SCHEMA"),
-    )
-"""
-
 @pytest.fixture
 def db_manager():
     datasource_db_port = os.getenv("DATASOURCE_DB_PORT")
