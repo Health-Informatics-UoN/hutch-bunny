f5b282295948964b3ce1309e8b547606a5dd7cf3
Andy Rae <hello@rae.li>
2025-02-09 22:22:36 +0000

    Improve logging and version reporting in CLI and daemon

--- src/hutch_bunny/core/logger.py
+++ src/hutch_bunny/core/logger.py
@@ -1,14 +1,16 @@
 import logging
-import hutch_bunny.core.settings as settings
 import sys
+from hutch_bunny.core.settings import get_settings
+
+settings = get_settings()
 
 logger = logging.getLogger(settings.LOGGER_NAME)
-LOG_FORMAT = logging.Formatter(
-    settings.MSG_FORMAT,
-    datefmt=settings.DATE_FORMAT,
-)
-console_handler = logging.StreamHandler(sys.stdout)
-console_handler.setFormatter(LOG_FORMAT)
-logger = logging.getLogger(settings.LOGGER_NAME)
-logger.setLevel(settings.LOGGER_LEVEL)
-logger.addHandler(console_handler)
+if not logger.hasHandlers():
+    LOG_FORMAT = logging.Formatter(
+        settings.MSG_FORMAT,
+        datefmt=settings.DATE_FORMAT,
+    )
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(LOG_FORMAT)
+    logger.setLevel(getattr(logging, settings.LOGGER_LEVEL.upper(), logging.INFO))
+    logger.addHandler(console_handler)
