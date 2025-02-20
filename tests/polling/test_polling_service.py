3cf613a3a8c2a4d5eb3ed04d2195c0966ab05ae7
Andy Rae <hello@rae.li>
2025-02-20 19:21:46 +0000

    Add error handling tests for polling service

--- tests/polling/test_polling_service.py
+++ tests/polling/test_polling_service.py
@@ -3,13 +3,14 @@ from unittest.mock import Mock, patch
 from hutch_bunny.core.polling.polling_service import PollingService
 from hutch_bunny.core.polling.task_api_client import TaskApiClient
 from logging import Logger
+import requests
 
 
 @pytest.fixture
 def mock_settings():
     mock_settings = Mock()
-    mock_settings.COLLECTION_ID = 'test_collection'
-    mock_settings.TASK_API_TYPE = 'test_type'
+    mock_settings.COLLECTION_ID = "test_collection"
+    mock_settings.TASK_API_TYPE = "test_type"
     mock_settings.INITIAL_BACKOFF = 1
     mock_settings.MAX_BACKOFF = 8
     mock_settings.POLLING_INTERVAL = 0.1
@@ -38,7 +39,9 @@ def test_poll_for_tasks_success(
     mock_client.get.return_value.status_code = 200
     mock_client.get.return_value.json.return_value = {"task": "data"}
 
-    polling_service = PollingService(mock_client, mock_task_handler, mock_settings, mock_logger)
+    polling_service = PollingService(
+        mock_client, mock_task_handler, mock_settings, mock_logger
+    )
 
     # Act
     with patch("time.sleep", return_value=None):  # To speed up the test
@@ -55,7 +58,9 @@ def test_poll_for_tasks_no_task(
     # Arrange
     mock_client.get.return_value.status_code = 204
 
-    polling_service = PollingService(mock_client, mock_task_handler, mock_settings, mock_logger)
+    polling_service = PollingService(
+        mock_client, mock_task_handler, mock_settings, mock_logger
+    )
 
     # Act
     with patch("time.sleep", return_value=None):  # To speed up the test
@@ -70,7 +75,9 @@ def test_construct_polling_endpoint_with_type(
     mock_settings, mock_client, mock_logger, mock_task_handler
 ):
     # Arrange
-    polling_service = PollingService(mock_client, mock_task_handler, mock_settings, mock_logger)
+    polling_service = PollingService(
+        mock_client, mock_task_handler, mock_settings, mock_logger
+    )
 
     # Act
     endpoint = polling_service._construct_polling_endpoint()
@@ -87,10 +94,59 @@ def test_construct_polling_endpoint_without_type(
     mock_settings.COLLECTION_ID = "test_collection"
     mock_settings.TASK_API_TYPE = None
 
-    polling_service = PollingService(mock_client, mock_task_handler, mock_settings, mock_logger)
+    polling_service = PollingService(
+        mock_client, mock_task_handler, mock_settings, mock_logger
+    )
 
     # Act
     endpoint = polling_service._construct_polling_endpoint()
 
     # Assert
     assert endpoint == "task/nextjob/test_collection"
+
+
+@patch("time.sleep", return_value=None)
+def test_unauthorized_status_code(
+    mock_client, mock_logger, mock_task_handler, mock_settings
+):
+    # Arrange
+    mock_client.get.return_value.status_code = 401
+    polling_service = PollingService(
+        mock_client, mock_task_handler, mock_settings, mock_logger
+    )
+
+    # Act
+    polling_service.poll_for_tasks(max_iterations=1)
+
+    # Assert
+    mock_logger.info.assert_called_with("Failed to authenticate with task server.")
+
+
+@patch("time.sleep", return_value=None)
+def test_other_status_code(mock_client, mock_logger, mock_task_handler, mock_settings):
+    # Arrange
+    mock_client.get.return_value.status_code = 500
+    polling_service = PollingService(
+        mock_client, mock_task_handler, mock_settings, mock_logger
+    )
+
+    # Act
+    polling_service.poll_for_tasks(max_iterations=1)
+
+    # Assert
+    mock_logger.info.assert_called_with("Got http status code: 500")
+
+
+@patch("time.sleep", return_value=None)
+def test_network_error(mock_client, mock_logger, mock_task_handler, mock_settings):
+    # Arrange
+    mock_client.get.side_effect = requests.exceptions.RequestException("Network error")
+    polling_service = PollingService(
+        mock_client, mock_task_handler, mock_settings, mock_logger
+    )
+
+    # Act
+    polling_service.poll_for_tasks(max_iterations=1)
+
+    # Assert
+    mock_logger.error.assert_called_with("Network error occurred: Network error")
