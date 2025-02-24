0b76ec39feed0d0b5c4e0b59470692c3b15c6c21
Andy Rae <hello@rae.li>
2025-02-24 14:50:07 +0000

    Update daemon to inject settings

--- src/hutch_bunny/daemon.py
+++ src/hutch_bunny/daemon.py
@@ -1,7 +1,6 @@
 from hutch_bunny.core.settings import get_settings, DaemonSettings
-from hutch_bunny.core.db_manager import SyncDBManager
 from hutch_bunny.core.upstream.task_api_client import TaskApiClient
-from hutch_bunny.core.logger import logger
+from hutch_bunny.core.logger import setup_logger
 from hutch_bunny.core.setting_database import setting_database
 from hutch_bunny.core.upstream.polling_service import PollingService
 from importlib.metadata import version
@@ -12,14 +11,15 @@ def main() -> None:
     """
     Main function to start the daemon process.
     """
-    logger.info(f"Starting Bunny version {version('hutch_bunny')} ")
     settings: DaemonSettings = get_settings(daemon=True)
+    logger = setup_logger(settings)
+    logger.info(f"Starting Bunny version {version('hutch_bunny')} ")
     logger.debug("Settings: %s", settings.safe_model_dump())
 
     # Setting database connection
     db_manager = setting_database(logger=logger)
 
-    client = TaskApiClient()
+    client = TaskApiClient(settings=settings, logger=logger)
     polling_service = PollingService(
         client,
         lambda task_data: handle_task(task_data, db_manager, settings, logger, client),
