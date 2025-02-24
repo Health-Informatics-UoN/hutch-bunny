81cff21b186f52a5140a1ceab2ae2ea0fa49f03b
Andy Rae <hello@rae.li>
2025-02-24 14:47:32 +0000

    Fix logger setup

--- src/hutch_bunny/core/logger.py
+++ src/hutch_bunny/core/logger.py
@@ -1,16 +1,25 @@
 import logging
-from hutch_bunny.core.settings import get_settings
 import sys
+from logging import Logger
+from hutch_bunny.core.settings import Settings
 
-settings = get_settings()
+def setup_logger(settings: Settings) -> Logger:
+    """
+    Sets up a logger with the given settings.
 
-logger = logging.getLogger(settings.LOGGER_NAME)
-LOG_FORMAT = logging.Formatter(
-    settings.MSG_FORMAT,
-    datefmt=settings.DATE_FORMAT,
-)
-console_handler = logging.StreamHandler(sys.stdout)
-console_handler.setFormatter(LOG_FORMAT)
-logger = logging.getLogger(settings.LOGGER_NAME)
-logger.setLevel(settings.LOGGER_LEVEL)
-logger.addHandler(console_handler)
+    Args:
+        settings (Settings): The settings to use for the logger.
+
+    Returns:
+        Logger: The logger.
+    """
+    logger = logging.getLogger(settings.LOGGER_NAME)
+    LOG_FORMAT = logging.Formatter(
+        settings.MSG_FORMAT,
+        datefmt=settings.DATE_FORMAT,
+    )
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(LOG_FORMAT)
+    logger.setLevel(settings.LOGGER_LEVEL)
+    logger.addHandler(console_handler)
+    return logger
