be8f829f7d3ff6fa37da67d74497ff8d5d12e70a
Andy Rae <hello@rae.li>
2025-06-08 17:20:22 +0100

    Enhance docstrings in Rule model for improved clarity and understanding of attributes and methods

--- src/hutch_bunny/core/rquest_models/rule.py
+++ src/hutch_bunny/core/rquest_models/rule.py
@@ -4,16 +4,81 @@ from pydantic import BaseModel, Field
 
 
 class Rule(BaseModel):
-    """Rule"""
+    """
+    A rule in a group of rules.
+
+    Specifies the search criteria for a rule.
+    """
+
+    varname: str = ""
+    """
+    Variable name to search for.
+
+    Either:
+    - `OMOP`: For OMOP searches
+    - `AGE`: For AGE searches
+    - `OMOP=21490742`: For Measurement searches
+    """
+
+    varcat: Literal["Person", "Condition", "Observation", "Drug", "Measurement"]
+    """
+    Table to search in.
+    """
+
+    type_: Literal["NUM", "TEXT"] = Field(default="TEXT", alias="type")
+    """
+    Type of value to search for.
+
+    - `TEXT`: For OMOP concept_id searches
+    - `NUM`: For AGE or Measurement searches
+    """
+
+    operator: Literal["=", "!="] = Field(default="=", alias="oper")
+    """
+    Operator to use in the search
+
+    = for inclusion
+
+    != for exclusion
+    """
 
     value: str = ""
-    type_: Literal["NUM", "TEXT"] = Field(default="TEXT", alias="type")
+    """
+    Value to search for.
+
+    TEXT searches have a OMOP concept_id (for example `8507`)
+
+    NUM searches have a range value split by `|` (for example 1.0|3.0)
+    """
+
     time: str | None = None
-    varname: str = ""
-    operator: Literal["=", "!="] = Field(default="=", alias="oper")
-    raw_range: str | None = None
-    varcat: Literal["Person", "Condition", "Observation", "Drug", "Measurement"]
+    """
+    Time to search for.
+
+    A time is a number followed by a colon and a unit.
+
+    If the `|` is on the left of the value it was less than the number.
+
+    If the `|` is on the right of the value it was greater than the number.
+
+    For example:
+    - 10|:AGE:Y (greater than 10 years)
+    - 10|:TIME:M (greater than 10 months)
+    - |10:TIME:M (less than 10 months)
+    - |10:AGE:Y (less than 10 years)
+    """
+
     secondary_modifier: list[int] | None = None
+    """
+    Secondary modifier to use in the search.
+
+    A list of concept_ids, for example `[32020]`.
+    """
+
+    """
+    Used to store parsed numeric values from range strings.
+    """
+    raw_range: str | None = None
     min_value: float | None = None
     max_value: float | None = None
 
@@ -23,7 +88,15 @@ class Rule(BaseModel):
     }
 
     def model_post_init(self, __context: Any) -> None:
-        """Initialize numeric values after model creation"""
+        """
+        Initialize numeric values after model creation
+
+        Args:
+            __context: The context of the model creation.
+
+        Returns:
+            None
+        """
         if self.type_ == "NUM":
             self.min_value, self.max_value = self._parse_numeric(self.value)
             parts = self.varname.split("=")
@@ -35,7 +108,15 @@ class Rule(BaseModel):
 
     @staticmethod
     def _parse_numeric(value: str) -> tuple[float | None, float | None]:
-        """Parse numeric values from range strings"""
+        """
+        Parse numeric values from range strings.
+
+        Args:
+            value (str): The value to parse.
+
+        Returns:
+            tuple[float | None, float | None]: The parsed numeric values.
+        """
         pattern = re.compile(r"(-?\d*\.\d+|\d+|null)\.\.(-?\d*\.\d+|null)")
         if match := re.search(pattern, value):
             lower, upper = match.groups()
