b28fd746b4519a2bf15cab18605120a27ffca33b
James Cockayne <james.cockayne@answerdigital.com>
2025-09-16 16:32:28 +0100

    Fix merge error

--- src/hutch_bunny/core/db/__init__.py
+++ src/hutch_bunny/core/db/__init__.py
@@ -15,7 +15,6 @@ from .sync import SyncDBClient
 from .trino import TrinoDBClient
 from .azure import AzureManagedIdentityDBClient
 from .duckdb import DuckDBClient
-from .duckdb import DuckDBClient
 from .utils import (
     DEFAULT_TRINO_PORT,
     expand_short_drivers,
@@ -70,16 +69,6 @@ def _create_duckdb_client() -> DuckDBClient:
         schema=settings.DATASOURCE_DB_SCHEMA,
         duckdb_temp_directory=settings.DATASOURCE_DUCKDB_TEMP_DIRECTORY,
     )
-
-def _create_duckdb_client() -> DuckDBClient:
-    """Create an DuckDB client."""
-
-    return DuckDBClient(
-        path_to_db=settings.DATASOURCE_DUCKDB_PATH_TO_DB,
-        duckdb_memory_limit=settings.DATASOURCE_DUCKDB_MEMORY_LIMIT,
-        schema=settings.DATASOURCE_DB_SCHEMA,
-    )
-
 
 def _create_sync_client() -> SyncDBClient:
     """Create a regular synchronous database client."""
