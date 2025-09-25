f6deb715e548515f2ebd62367234c2aadc5241fe
Benjamin Partridge <bp1183@my.bristol.ac.uk>
2025-09-25 09:05:01 +0100

    Added tempo, otel-collector, grafana  config yamls, updated observability compose to include tempo for tracing

--- src/hutch_bunny/core/telemetry.py
+++ src/hutch_bunny/core/telemetry.py
@@ -32,6 +32,8 @@ def setup_telemetry(settings: Settings) -> None:
 
         _setup_logging_integration(resource, settings)
 
+        _setup_tracing(resource, settings)
+
         SQLAlchemyInstrumentor().instrument()
         RequestsInstrumentor().instrument()
 
